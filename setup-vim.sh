d0b08621fa0db62f74416045d0d9e434226349be
Maximiliana Behnke <mbehnke@microsoft.com>
2025-06-11 17:31:13 +0000

    modify setup vim

--- setup-vim.sh
+++ setup-vim.sh
@@ -7,7 +7,7 @@ sudo apt -y install luarocks cargo ripgrep
 rm -rf neovim
 git clone https://github.com/neovim/neovim.git -b release-0.11
 cd neovim
-sudo rm -r build/  # clear the CMake cache
+sudo rm -rf build/  # clear the CMake cache
 make distclean
 make -j16  CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo rm -rf /usr/local/share/nvim/runtime
@@ -22,6 +22,7 @@ git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
 # remove template's git connection to set up your own later
 rm -rf ~/.config/nvim/.git
 cp -r ./config/nvim ~/.config/
+sudo cp -r runtime /usr/local/share/nvim
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
